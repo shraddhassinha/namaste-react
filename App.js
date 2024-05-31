773611834077a690058119458dcfdd3373959de2
Shraddha Sinha <shraddhasinha@Shraddhas-MacBook-Air.local>
2024-05-31 16:25:27 +0530

    feature-finalCode

--- App.js
+++ App.js
@@ -1,24 +1,59 @@
-{
-  /* <div class="parent">
-    <div class="child">
-        <h1>
-I am first child
-        </h1>
-        <h1>
-I am second child
-        </h1>
-    </div>
-</div> */
-}
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import HeaderComponent from "./src/components/HeaderComponent";
+import Body from "./src/components/Body";
+import About from "./src/components/About";
+import Contact from "./src/components/Contact";
+import ErrorComponent from "./src/components/ErrorComponent";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import RestaurantMenu from "./src/components/RestaurantMenu";
+import UserContext from "./src/utils/UserContext";
+import appStore from "./src/utils/store";
+import Cart from "./src/components/Cart";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am first child"),
-    React.createElement("h2", {}, "I am second child"),
-  ])
-);
-const element = React.createElement("h1", {}, "Hello World from react");
+const AppLayout = () => {
+  const [userName, setUserName] = useState("Priyanka Sinha");
+  return (
+    <Provider store={appStore}>
+      <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
+        <div className="app-container">
+          <HeaderComponent />
+          <Outlet />
+          ----Footer----
+        </div>
+      </UserContext.Provider>
+    </Provider>
+  );
+};
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <ErrorComponent />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurant/:resId",
+        element: <RestaurantMenu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart/>,
+      },
+    ],
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<RouterProvider router={appRouter} />);
